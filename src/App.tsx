c1585bb977804293669708bf372f50170eb1bac8
r-b-1 <null012525@gmail.com>
2026-02-08 18:15:44 -0600

    new node + vite app!

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Hello from my macOS app 🚀</h1>
+      <p>This will run on the web and macOS.</p>
+    </div>
+  );
+}
+
+export default App;
